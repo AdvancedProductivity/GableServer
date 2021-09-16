37ce55c8b9fd47d550cf29849b488dbc6132173b
BreakZhao <1694392889@qq.com>
2021-09-16 18:08:39 +0800

    refactor(package): refactor package structure
    Closes #7

--- src/main/java/org/advancedproductivity/gable/web/controller/PageController.java
+++ src/main/java/org/advancedproductivity/gable/web/controller/PageController.java
@@ -1,4 +1,4 @@
-package org.advancedproductivity.gable.web.controller.test;
+package org.advancedproductivity.gable.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
