6d1181020c9f0719b54e7fedb7c4d72036761ec8
BreakZhao <1694392889@qq.com>
2021-09-16 17:25:36 +0800

    feat(cross): CrossFilter
    Closes #4

---
+++ src/main/java/org/advancedproductivity/gable/web/config/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package org.advancedproductivity.gable.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                        .allowCredentials(true).maxAge(3600);
+            }
+        };
+    }
+}
