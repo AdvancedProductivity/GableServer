55274b20eb6ae1d41608ccab8059ec188a683c87
BreakZhao <1694392889@qq.com>
2021-09-27 11:14:21 +0800

    refactor(auth): remove custom auth impl
    Closes

--- src/main/java/org/advancedproductivity/gable/framework/auth/AuthHolder.java
+++ src/main/java/org/advancedproductivity/gable/framework/auth/AuthHolder.java
@@ -7,7 +7,5 @@ public class AuthHolder {
     public static final ConcurrentHashMap<String, AuthHandler> HOLDER = new ConcurrentHashMap<>();
 
     static {
-        WallTechAuth wallTechAuth = new WallTechAuth();
-        HOLDER.put(wallTechAuth.getAuthType().name(), wallTechAuth);
     }
 }
