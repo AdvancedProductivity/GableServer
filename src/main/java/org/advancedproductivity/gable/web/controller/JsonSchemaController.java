0824a9e3ac6a647bb3dbb9cbeb98c7c7d0144a2d
BreakZhao <1694392889@qq.com>
2021-10-14 19:43:04 +0800

    feat(JsonSchema): Run Validate api
    Closes #

--- src/main/java/org/advancedproductivity/gable/web/controller/JsonSchemaController.java
+++ src/main/java/org/advancedproductivity/gable/web/controller/JsonSchemaController.java
@@ -1,14 +1,27 @@
 package org.advancedproductivity.gable.web.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersionDetector;
+import com.networknt.schema.ValidationMessage;
 import com.saasquatch.jsonschemainferrer.*;
+import org.advancedproductivity.gable.framework.config.GableConfig;
 import org.advancedproductivity.gable.framework.config.HttpResponseField;
 import org.advancedproductivity.gable.framework.core.TestType;
+import org.advancedproductivity.gable.framework.utils.GableFileUtils;
 import org.advancedproductivity.gable.web.entity.Result;
+import org.advancedproductivity.gable.web.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.Set;
 
 /**
  * @author zzq
@@ -16,6 +29,7 @@ import java.util.EnumSet;
 @RestController
 @RequestMapping("/api/jsonSchema")
 public class JsonSchemaController {
+    private static final String JSON_SCHEMA_FILE_NAME = "jsonSchemaRecord.json";
     private static final JsonSchemaInferrer inferrer = JsonSchemaInferrer.newBuilder()
             .setSpecVersion(SpecVersion.DRAFT_07)
             // Requires commons-validator
@@ -35,5 +49,52 @@ public class JsonSchemaController {
             return Result.success(inferrer.inferForSample(body));
         }
         return Result.success(inferrer.inferForSample(in));
+    }
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private HttpServletRequest request;
+
+    @Resource
+    private ObjectMapper objectMapper;
+
+    @GetMapping
+    public Result getConfig() {
+        String userId = userService.getUserId(null, request);
+        JsonNode node = null;
+        if (!StringUtils.isEmpty(userId)) {
+            node = GableFileUtils.readFileAsJson(GableConfig.getGablePath(), userId, JSON_SCHEMA_FILE_NAME);
+        }
+        if (node == null) {
+            return Result.error();
+        }
+        return Result.success(node);
+    }
+
+    @PutMapping
+    public Result validate(@RequestBody JsonNode jsonNode) {
+        JsonNode schema = jsonNode.path("schema");
+        JsonNode json = jsonNode.path("json");
+        String userId = userService.getUserId(null, request);
+        if (StringUtils.isEmpty(userId)) {
+            return Result.error("validate error");
+        }
+        if (!schema.isObject()) {
+            return Result.error("JsonSchema格式不正确");
+        }
+        if (json.isMissingNode()) {
+            return Result.error("json格式不正确");
+        }
+        JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersionDetector.detect(schema));
+        JsonSchema validator = factory.getSchema(schema);
+        Set<ValidationMessage> validate = validator.validate(json);
+        ArrayNode arrayNode = objectMapper.createArrayNode();
+        for (ValidationMessage validationMessage : validate) {
+            arrayNode.add(validationMessage.getMessage());
+        }
+        GableFileUtils.saveFile(jsonNode.toPrettyString(), GableConfig.getGablePath(), userId, JSON_SCHEMA_FILE_NAME);
+        return Result.success(arrayNode);
     }
 }
